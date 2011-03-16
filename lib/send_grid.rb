bd92c76215c73b31b75c05a9b7b5cac760fd3534
PavelT <paveltyk@gmail.com>
2011-03-16 20:24:58 +0200

    VERSION added to aoutoload

--- lib/send_grid.rb
+++ lib/send_grid.rb
@@ -3,5 +3,6 @@ require 'active_support/json'
 
 module SendGrid
   autoload :ApiHeader, 'send_grid/api_header'
+  autoload :VERSION, 'send_grid/version'
 end
 
---
+++ spec/version_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe SendGrid::VERSION do
+  it "returns string" do
+    SendGrid::VERSION.should be_an_instance_of(String)
+  end
+end
+
