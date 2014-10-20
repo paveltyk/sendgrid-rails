899c3529cdf412fbf279e9e32b2b0d4ac487d70c
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2014-10-20 11:16:59 -0400

    Include CC and BCC in mail interceptor.

--- lib/send_grid/mail_interceptor.rb
+++ lib/send_grid/mail_interceptor.rb
@@ -3,6 +3,8 @@ module SendGrid
     def self.delivering_email(mail)
       sendgrid_header = mail.instance_variable_get(:@sendgrid_header)
       sendgrid_header.add_recipients(mail.to)
+      sendgrid_header.add_recipients(mail.cc)
+      sendgrid_header.add_recipients(mail.bcc)
       mail.header['X-SMTPAPI'] = sendgrid_header.to_json if sendgrid_header.data.present?
       mail.header['to'] = SendGrid.config.dummy_recipient
     end
